3bf6d18b3951208dfcfb0c534870fa61555c08a9
aitor <gmaitor@outlook.com>
2020-11-17 01:33:29 +0100

    getting lowest price from cdkeybay

--- frp-scraping.js
+++ frp-scraping.js
@@ -13,14 +13,30 @@ function ElementData(args) {
     this.name = args.name
 }
 
-async function main(){
+async function scrapeInstantGaming(){
     try{
         const url = 'https://www.instant-gaming.com/en/search/?query=sekiro'
         const html = await webRequest(url);
         const $ = cheerio.load(html);
-        const div_search_children = getDomTree($);
-        const elementsData = getElementsData($, div_search_children);
+        const div_search_children = getInstantGamingDomTree($);
+        const elementsData = getInstantGamingElementsData($, div_search_children);
         console.log(elementsData);
+    }catch(err){
+        console.log('WASAP',err);
+    }
+    
+}
+async function scrapeCDKEYBAY(){
+    try{
+        const url = 'https://www.cdkeybay.com/search/sekiro-shadows-die-twice'
+        const html = await webRequest(url);
+        const $ = cheerio.load(html);
+        const rawDAta = getInstantCDKEYBAYTree($);
+        const allGames = cleanWebData(rawDAta);
+        const cheapestGame = getCheapestGame(allGames)
+
+        console.log('CHEAPESTGAME', cheapestGame);
+
     }catch(err){
         console.log('WASAP',err);
     }
@@ -37,13 +53,13 @@ async function webRequest(url) {
     }
 }
 
-function getDomTree($){
-    return $('div[class="search"]').children();
+function getInstantCDKEYBAYTree($){
+    return $('main[id="m"]').children().first().html();
 }
 
-function getElementsData($, div_search_children){
+function getInstantGamingElementsData($, div_search_children){
     const elementsData = [];
-    div_search_children.map(function (i, el) {
+    div_search_children.map((i, el) => {
         const obj = {
             region: $(el).attr('data-region'),
             price: $(el).attr('data-price'),
@@ -68,4 +84,19 @@ function getElementsData($, div_search_children){
     return elementsData;
 }
 
-main()
+function cleanWebData(rawDAta){
+    const stringArray = rawDAta.split(' = ')[1]
+    return JSON.parse(stringArray)
+}
+function getCheapestGame(allGames){
+    const checkPlatformAndZone = (args)=> {
+        const platformValidation = args.platform.toLowerCase().includes('steam');
+        const zoneValidation = args.zone.toLowerCase().includes('europe') || args.zone.toLowerCase().includes('global');
+
+        return platformValidation&&zoneValidation;
+    }
+    const filteredGame = allGames.filter(checkPlatformAndZone);
+    return filteredGame.sort((a, b) => a.price - b.price)[0];
+}
+
+scrapeCDKEYBAY()
